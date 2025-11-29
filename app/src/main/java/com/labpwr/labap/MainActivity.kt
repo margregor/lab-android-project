03f21606f1820a18f1f5a63428178b59dd13c84e
Marcin Grzegorzewski <82527766+margregor@users.noreply.github.com>
2025-11-29 23:48:57 +0100

    Initial commit

---
+++ app/src/main/java/com/labpwr/labap/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.labpwr.labap
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.labpwr.labap.ui.theme.LabapTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LabapTheme {
+                Column( modifier = Modifier.fillMaxWidth(1f) ) {
+                    Spacer(modifier = Modifier.height(40.dp))
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(1.dp)
+                    )
+                    Spacer(modifier = Modifier.height(20.dp))
+                    Button(
+                        modifier = Modifier.padding(1.dp),
+                        onClick = {
+                            Intent(this@MainActivity, NativeLoader::class.java).apply {
+                                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                                startActivity(this)
+                            }
+                        }
+                    ) {
+                        Text(text = "Game1")
+                    }
+                    Button(
+                        modifier = Modifier.padding(1.dp),
+                        onClick = {
+
+                        }
+                    ) {
+                        Text(text = "Game2")
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LabapTheme {
+        Greeting("Android")
+    }
+}
